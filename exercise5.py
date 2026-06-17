058a0e7035fd7bf1b37e0508aa3b903c04ee1514
Trong_Minh <trongminhmcw10e@gmail.com>
2026-06-17 21:02:54 +0700

    Update training ex5

--- exercise5.py
+++ exercise5.py
@@ -8,8 +8,6 @@ from torch.nn import functional as F
 from torchvision import datasets, transforms
 from tqdm import tqdm
 
-from exercise5 import train_loader
-
 transform = transforms.Compose([transforms.ToTensor()])
 train_dataset = datasets.MNIST(
     root="./data", train=True, download=True, transform=transform
@@ -19,11 +17,15 @@ test_dataset = datasets.MNIST(
 )
 
 train_loader = torch.utils.data.DataLoader(
-    train_dataset.data.unsqueeze(1), batch_size=128, shuffle=True
+    torch.utils.data.TensorDataset(train_dataset.data.unsqueeze(1)),
+    batch_size=128,
+    shuffle=True,
 )
 
 test_loader = torch.utils.data.DataLoader(
-    test_dataset.data.unsqueeze(1), batch_size=128, shuffle=True
+    torch.utils.data.TensorDataset(test_dataset.data.unsqueeze(1)),
+    batch_size=128,
+    shuffle=True,
 )
 
 
@@ -74,12 +76,35 @@ class ConvolutionalAutoEncoder(nn.Module):
 
 
 def train_model(
-    model: nn.Module, criterion=None, optimizer=None, dataloader=train_loader
+    model: nn.Module,
+    criterion: nn.Module,
+    optimizer: torch.optim.Optimizer,
+    dataloader: torch.utils.data.DataLoader = train_loader,
+    epoch=10,
+    device="cuda",
 ) -> nn.Module:
+    model = model.to(device)
+    model.train()
+    for e in range(epoch):
+        pbar = tqdm(enumerate(dataloader))
+        pbar.set_description(f"Training model using {device}")
+        for batch_idx, (data, target) in pbar:
+            data, target = data.to(device), target.to(device)
 
+            optimizer.zero_grad()
+            out = model(data)
+            loss = criterion(out, target)
+            loss.backward()
+            optimizer.step()
+
+            pbar.set_description(f"Loss: {loss.item()}")
     return model
 
 
-def test_model(model: nn.Module, dataloader: test_loader) -> None:
-
+def test_model(
+    model: nn.Module, dataloader: torch.utils.data.DataLoader = test_loader
+) -> None:
+    model.eval()
+    with torch.no_grad():
+        pass
     return
